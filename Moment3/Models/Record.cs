7076c2d9f348825ae3f858e592009704b99ec6c5
Joacim Bäcklund <joba1905@student.miun.se>
2023-02-10 21:23:09 +0100

    Data annotations added!

--- Moment3/Models/Record.cs
+++ Moment3/Models/Record.cs
@@ -6,7 +6,11 @@ namespace Moment3.Models
     public class Record
     {
         public int RecordID { get; set; }
+
+        [Required(ErrorMessage = "A CD title is required!")]
         public string? Title { get; set; }
+
+        [Required(ErrorMessage = "The release year is required!")]
         public string? Year { get; set; }
 
         [Display(Name = "Artist")]
